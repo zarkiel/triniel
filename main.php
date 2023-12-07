4aec18c2f55ccfb74660f562b7798dc77eaafd9d
Zarkiel <zarkiel@gmail.com>
2023-12-07 15:57:57 -0500

    Attributes changed to Strins, no definitions required

--- main.php
+++ main.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require_once 'attributes.php';
 require_once 'exceptions.php';
 require_once 'router.php';
 require_once 'oas_creator.php';
